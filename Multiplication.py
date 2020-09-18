db681b875ee6dd8fc8abfe23f3c3ce6aa31262b0
deenanathS <deena.srivastava18@st.niituniversity.in>
2020-09-18 09:46:39 +0530

    Pushed the Multiplication File

---
+++ Multiplication.py
@@ -0,0 +1,11 @@
+#Multiplication Code
+
+def multiply(x,y):
+	
+	result = x*y
+	return result
+
+a= float(input("Enter the Ist Number: "))
+b= float(input("Enter the Second Number: "))
+
+print("The Multiplication of the two numbers is: " multiply(a,b))
